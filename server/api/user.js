118d33f1699635e0ddcfc9e59766d6aac741dc4e
no92one <linus.lindroth.92@gmail.com>
2024-02-02 15:48:07 +0100

    Added cryptation

--- server/api/user.js
+++ server/api/user.js
@@ -1,4 +1,12 @@
+import { log } from "console";
 import User from "../models/User.js";
+import crypto from "crypto"
+const salt = "paraplane"
+
+function getHash(password) {
+  let hash = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`)
+  return hash
+}
 
 export default function (server) {
 
@@ -6,7 +14,7 @@ export default function (server) {
     try {
       const newUser = new User({
         username: req.body.username,
-        password: req.body.password
+        password: getHash(req.body.password)
       })
       const savedUser = await newUser.save()
       res.status(201).json(savedUser)
@@ -22,17 +30,26 @@ export default function (server) {
     } else {
       const user = await User.findOne({
         username: req.body.username,
-        password: req.body.password
+        password: getHash(req.body.password)
       })
 
       if (user) {
         req.session.login = user._id
-        res.json({ mysession: req.session, message: `Du har lyckats logga in som ${user.username}.` })
+        res.json({ message: `Du har lyckats logga in som ${user.username}.` })
       } else {
         res.json({ message: "Hitta ingen användare. Fel användarnamn eller lösenord." })
       }
     }
   })
 
+  server.delete("/api/login", async (req, res) => {
+    if (req.session.login) {
+      const user = await User.findById(req.session.login)
+      delete req.session.login
+      res.json({ message: `Hejdå ${user.username}! Du har loggat ut. Ha en fortsatt bra dag!` })
+    } else {
+      res.json({ message: "Går inte att logga ut när ingen är inloggad." })
+    }
+  })
 
 }
